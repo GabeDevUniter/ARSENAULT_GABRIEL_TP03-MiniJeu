a5b1bcecc6fce881a6f58c92ca6edf982a32dda9
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-04-14 09:04:41 -0400

    Added FX_AutoDestruct.cs from TP02
    Added the script that auto destructs particles, which I forgot to add

---
+++ Assets/Scripts/FX_AutoDestruct.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+/// <summary>
+/// Automatically destroys a particle effect when finished
+/// </summary>
+public class FX_AutoDestruct : MonoBehaviour
+{
+    private float time;
+
+    private ParticleSystem particle;
+
+    private void Awake()
+    {
+        particle = GetComponent<ParticleSystem>();
+
+        time = particle.main.duration;
+
+        StartCoroutine(Destroy());
+    }
+
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(time);
+
+        Destroy(gameObject);
+    }
+}
