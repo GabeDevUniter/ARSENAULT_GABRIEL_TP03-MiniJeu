704ba14b7b353961493c7d81042eb0806e267c22
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-04-21 08:53:53 -0400

    Added InteractionController script
    Added script for object interaction such as doors, weapons, etc.

---
+++ Assets/Scripts/Interactable/InteractionController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InteractTypes { Weapon, Door}
+
+public class InteractionController : MonoBehaviour
+{
+    [SerializeField]
+    private Collider interactCollider;
+
+    [SerializeField]
+    private InteractTypes interactType;
+
+    [SerializeField]
+    private float distance = 50f;
+
+    private Camera mainCam;
+
+    private GameObject interactGameObject;
+
+    void Awake()
+    {
+        mainCam = Camera.main;
+
+        interactGameObject = interactCollider.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(interactGameObject.transform.position, GameManager.singleton.PlayerHead) <= distance)
+        {
+
+        }
+    }
+}
