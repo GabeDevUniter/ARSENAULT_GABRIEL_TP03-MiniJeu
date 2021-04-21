9f7e57cf0ce60a04203e3f4b0af80758b61d4fe6
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-04-21 11:26:17 -0400

    Update Door.cs
    Attempts to make the door rotate. It can't do that right

--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -21,13 +21,15 @@ public class Door : MonoBehaviour
     public void Open()
     {
         if (!isMoving && !currentState)
-            StartCoroutine(DoorMove(doorTransform.rotation.y, doorTransform.rotation.y - orientation));
+            StartCoroutine(DoorMove2(doorTransform.rotation, new Quaternion(doorTransform.rotation.x, doorTransform.rotation.y - orientation * Mathf.Deg2Rad, 0f, 0f)));
+        //StartCoroutine(DoorMove(doorTransform.rotation.eulerAngles.y, doorTransform.rotation.eulerAngles.y - orientation));
     }
 
     public void Close()
     {
         if (!isMoving && currentState)
-            StartCoroutine(DoorMove(doorTransform.rotation.y, doorTransform.rotation.y + orientation));
+            StartCoroutine(DoorMove2(doorTransform.rotation, new Quaternion(doorTransform.rotation.x, doorTransform.rotation.y + orientation * Mathf.Deg2Rad, 0f, 0f)));
+        //StartCoroutine(DoorMove(doorTransform.rotation.eulerAngles.y, doorTransform.rotation.eulerAngles.y + orientation));
     }
 
     public void Toggle()
@@ -43,21 +45,63 @@ public class Door : MonoBehaviour
     {
         isMoving = true;
 
-        while(doorTransform.rotation.y <= start + (end - start))
+        float direction = (end - start) / Mathf.Abs(end - start);
+
+        Debug.Log(doorTransform.rotation.y);
+        Debug.Log(end * Mathf.Deg2Rad);
+        Debug.Log(doorTransform.rotation.y + end * Mathf.Deg2Rad);
+
+        Debug.Log(doorTransform.rotation.eulerAngles.y);
+        Debug.Log(start + (end - start));
+        while(doorTransform.rotation.eulerAngles.y >= start && doorTransform.rotation.eulerAngles.y <= end)
         {
             float elapsed = 0f;
 
             while (elapsed <= 1f)
             {
-                doorTransform.Rotate(Vector3.up, speed * Time.deltaTime);
+                doorTransform.Rotate(new Vector3(0f, speed * Time.deltaTime * direction, 0f));
 
                 elapsed += Time.deltaTime;
 
+                Debug.Log("In elapsed");
+
                 yield return null;
             }
+
+            Debug.Log("In progress");
         }
 
-        doorTransform.rotation = new Quaternion(doorTransform.rotation.x, end, doorTransform.rotation.z, doorTransform.rotation.w);
+        Quaternion endResult = doorTransform.rotation;
+        endResult.eulerAngles = new Vector3(0f, end, 0f);
+
+        //doorTransform.Rotate(Vector3.up, end - doorTransform.rotation.y);
+        
+        currentState = !currentState;
+
+        isMoving = false;
+    }
+
+    IEnumerator DoorMove2(Quaternion start, Quaternion end)
+    {
+        isMoving = true;
+
+        Debug.Log(start);
+        Debug.Log(end);
+
+        float duration = (end.y - start.y) / speed;
+
+        float elapsed = 0f;
+
+        while(elapsed <= duration)
+        {
+            doorTransform.rotation = Quaternion.Lerp(start, end, elapsed / duration);
+
+            yield return null;
+        }
+
+        doorTransform.rotation = end;
+
+        currentState = !currentState;
 
         isMoving = false;
     }
