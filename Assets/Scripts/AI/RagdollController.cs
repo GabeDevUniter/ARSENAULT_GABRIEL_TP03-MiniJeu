343765b9ae90f95b1cebf90c2392b1c674bfa93d
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-05 12:44:20 -0400

    Added RagdollController
    Added RagdollController script to manage the NPC's ragdoll instead of the Grunt script managing the ragdoll. A cooldown is also available for freezing the ragdoll and disabling the colliders after five seconds

---
+++ Assets/Scripts/AI/RagdollController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    [SerializeField]
+    private float cooldownTime = 5f;
+
+    private Rigidbody[] ragdoll;
+
+    public Rigidbody[] Ragdoll { get { return ragdoll; } }
+
+    private Collider[] colliders;
+
+
+    void Awake()
+    {
+        ragdoll = GetComponentsInChildren<Rigidbody>();
+
+        colliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void SetRagdoll(bool mode)
+    {
+        foreach (Rigidbody bone in ragdoll)
+        {
+            if(bone != null) bone.isKinematic = !mode;
+        }
+    }
+
+    public void SetRagdoll(bool mode, bool collision)
+    {
+        SetRagdoll(mode);
+
+        foreach(Collider collider in colliders)
+        {
+            if(collider != null) collider.enabled = collision;
+        }
+    }
+
+    public void startCooldown()
+    {
+        if (isCooling) return;
+
+        StartCoroutine(cooldown());
+    }
+
+    bool isCooling = false;
+    IEnumerator cooldown()
+    {
+        isCooling = true;
+
+        yield return new WaitForSeconds(cooldownTime);
+
+        SetRagdoll(false, false);
+
+        isCooling = false;
+    }
+}
