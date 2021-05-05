1f001c40cb5694c11c991204838ea54e14b857da
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-05 17:13:44 -0400

    Texture script for cropping tiles
    This script, kindly provided by august_unity496 from the forums, is used to change the texture tiling based on the size of the meshes, which is useful for small blocks that have smaller textures than the bigger blocks in the blockout

---
+++ Assets/Scripts/GifTexture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Thanks august_unity496 for providing this script at https://answers.unity.com/questions/361573/how-to-make-a-texture-tile-and-not-stretch.html
+public class GifTexture : MonoBehaviour
+{
+    [SerializeField] private float tileX = 1;
+    [SerializeField] private float tileY = 1;
+
+    Mesh mesh;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        mesh = GetComponent<MeshFilter>().mesh;
+
+    }
+
+    void Update()
+    {
+        mat.mainTextureScale = new Vector2((mesh.bounds.size.x *
+transform.localScale.x) / 100 * tileX, (mesh.bounds.size.y * transform.localScale.y) / 100 * tileY);
+    }
+}
