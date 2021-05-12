30f7eb370b76947bf427b6a3762879d79a8ae041
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-12 02:48:23 -0400

    Added Trigger script
    Added Trigger script, which will perform certain actions to triggerable objects when triggered by a box collider or an InteractionController
    
    The Trigger component requires a target, which is any script that inherits from Triggerable. When a script is chosen, certain parameters will be displayed. For instance, if a Door script is set as a target, you can tell that door to either open or close when the Trigger is triggered.
    
    Some other triggerable scripts, such as a music player or a trigger when all enemies are killed, are to be added

---
+++ Assets/Scripts/Interactable/Trigger.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum DoorOutput { Open, Close, Toggle }
+
+public abstract class Triggerable : MonoBehaviour { }
+
+public class Trigger : MonoBehaviour
+{
+    public bool triggerOnce = true;
+
+    public Triggerable target;
+
+    [HideInInspector]
+    public DoorOutput doorOutput;
+
+    [HideInInspector]
+    public bool doorLockOnMove;
+
+    [HideInInspector]
+    public bool doorForceMove; // Door will move wether it's locked or not
+
+    private bool canTrigger = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            setTrigger();
+        }
+    }
+
+    public void setTrigger()
+    {
+        if (!canTrigger || target == null) return;
+        
+        if (target.GetType() == typeof(Door))
+        {
+            var door = (Door)target;
+
+            if(doorForceMove) door.locked = false;
+
+            switch (doorOutput)
+            {
+                case DoorOutput.Open: door.Open(); break;
+
+                case DoorOutput.Close: door.Close(); break;
+
+                case DoorOutput.Toggle: door.Toggle(); break;
+            }
+
+            door.locked = doorLockOnMove;
+        }
+
+        Debug.Log("TRIGGERED!");
+
+        canTrigger = !triggerOnce;
+    }
+}
+
+[CustomEditor(typeof(Trigger))]
+public class TriggerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var trigger = target as Trigger;
+
+        if (trigger.target != null)
+        {
+            //Debug.Log(trigger.target.GetType());
+
+            if (trigger.target.GetType() == typeof(Door))
+            {
+                trigger.doorOutput = (DoorOutput)EditorGUILayout.EnumPopup("Output", trigger.doorOutput);
+
+                trigger.doorLockOnMove = EditorGUILayout.Toggle("Lock On Move", trigger.doorLockOnMove);
+
+                trigger.doorForceMove = EditorGUILayout.Toggle("Force Move", trigger.doorForceMove);
+            }
+            
+        }
+    }
+}
