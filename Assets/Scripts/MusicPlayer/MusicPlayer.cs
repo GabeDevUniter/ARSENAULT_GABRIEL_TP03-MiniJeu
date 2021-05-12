805659bf1cc146ea32acd448f43c490fff0409c7
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-12 19:23:17 -0400

    Update MusicPlayer.cs
    Added startPlay, soundtrack and segments fields.
    
    Implemented the Play() and Stop() methods, along with the PlaySong() coroutine to play the soundtrack and the loop segments
    
    The idea is to loop through song segments (because the music provider split the song loop into multiple files and I'm too lazy to combine them in Audacity) with the AudioSource soundtrack as an entry point..
    
    Every AudioSource's loop and playOnAwake are disabled, as the script will take care of both situations.
    
    If no loop segments are assigned, the AudioSource assigned on the soundtrack field will play once. If you want one entire AudioClip as a looping music, just put it as one loop segment and leave the soundtrack field blank.

--- Assets/Scripts/MusicPlayer/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer/MusicPlayer.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private bool startPlay = false;
+
+    [Header("Song/Entry")]
+    [SerializeField]
+    private AudioSource soundtrack;
+
+    [Header("Loop")]
+    [SerializeField]
+    private AudioSource[] segments;
+
+    void Awake()
     {
-        
+        if (soundtrack == null) soundtrack = GetComponent<AudioSource>();
+
+        if(soundtrack != null)
+        {
+            soundtrack.loop = false;
+            soundtrack.playOnAwake = false;
+        }
+
+        foreach(AudioSource segment in segments)
+        {
+            segment.loop = false;
+            segment.playOnAwake = false;
+        }
+
+        if (startPlay) Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
-        
+        Stop();
+
+        StartCoroutine(PlaySong());
+    }
+
+    public void Stop()
+    {
+        StopAllCoroutines();
+
+        soundtrack.Stop();
+
+        foreach (AudioSource segment in segments) segment.Stop();
+    }
+
+    IEnumerator PlaySong()
+    {
+        if(soundtrack != null)
+        {
+            soundtrack.Play();
+
+            Debug.Log($"Now playing: {soundtrack.clip.name}");
+
+            yield return new WaitForSeconds(soundtrack.clip.length);
+        }
+
+        while(segments.Length > 0)
+        {
+            foreach(AudioSource segment in segments)
+            {
+                segment.Play();
+
+                Debug.Log($"Now playing: {segment.clip.name}");
+
+                yield return new WaitForSeconds(segment.clip.length);
+            }
+        }
     }
 }
