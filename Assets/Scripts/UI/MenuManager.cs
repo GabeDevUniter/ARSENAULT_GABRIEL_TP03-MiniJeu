ca509f785373e40a0b8815c95ab093c9403c4dc1
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-13 22:47:27 -0400

    Created MenuManager script
    Created MenuManager script

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    private Menu[] menus;
+
+    private void Awake()
+    {
+        menus = GetComponentsInChildren<Menu>();
+
+        CloseAllMenus();
+    }
+
+    public void CloseAllMenus()
+    {
+        foreach (Menu menu in GetComponentsInChildren<Menu>()) 
+            menu.Close();
+    }
+
+    public void OpenMenu(Menu menu)
+    {
+        CloseAllMenus();
+
+        menu.Open();
+    }
+}
