3631d3185a9fa20a2ad86cc41d9214f2445f6f64
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-14 01:44:07 -0400

    Added MainMenuAnimation script
    Added MainMenuAnimation script

---
+++ Assets/Scripts/UI/MainMenuAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuAnimation : MonoBehaviour
+{
+    [Header("Delay")]
+    [SerializeField]
+    private float startDelay = 3f;
+
+    [Header("Title")]
+    [SerializeField]
+    private Animator Title;
+
+    private float titleInterval = 1f;
+
+    [Header("Buttons")]
+    [SerializeField]
+    private Animator[] Buttons;
+
+    [SerializeField]
+    private float buttonInterval = 0.5f;
+
+    GraphicRaycaster gr;
+
+    void Awake()
+    {
+        gr = GetComponent<GraphicRaycaster>();
+
+        gr.enabled = false;
+
+        StartCoroutine(Animate());
+    }
+
+    IEnumerator Animate()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        Title.SetTrigger("Start");
+
+        yield return new WaitForSeconds(titleInterval);
+
+        for(int i = 0; i < Buttons.Length; i += 2) // +2 Because texts are separated from the button sprite
+        {
+            Buttons[i].SetTrigger("Start");
+            Buttons[i+1].SetTrigger("Start");
+
+            yield return new WaitForSeconds(buttonInterval);
+        }
+
+        gr.enabled = true;;
+    }
+    
+}
