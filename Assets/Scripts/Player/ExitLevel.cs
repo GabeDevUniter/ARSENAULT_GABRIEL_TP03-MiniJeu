431abfa0b30b53a6951619538eaf5a11a886eb4e
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-15 16:47:41 -0400

    Added ExitLevel script
    Added ExitLevel script for exiting the level when completed

---
+++ Assets/Scripts/Player/ExitLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitLevel : MonoBehaviour
+{
+    [SerializeField]
+    private Fade fade;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && GameManager.singleton.IsGameEnded) StartCoroutine(EndLevel());
+    }
+
+    IEnumerator EndLevel()
+    {
+        if(fade != null)
+        {
+            fade.FadeIn();
+
+            yield return new WaitForSeconds(fade.FadeInDuration + 2f);
+        }
+
+        GameManager.singleton.ChangeLevel("Main");
+    }
+}
