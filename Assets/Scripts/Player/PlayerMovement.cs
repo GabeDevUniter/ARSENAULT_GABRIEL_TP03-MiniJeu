191a3206e24f41f998b8f68c02d4767ef5e2c947
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-15 20:32:27 -0400

    Update PlayerMovement.cs
    Prevent the camera from turning when paused

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -73,7 +73,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        MoveCam();
+        if(!PauseMenu.IsPaused) MoveCam();
     }
 
     private void FixedUpdate()
