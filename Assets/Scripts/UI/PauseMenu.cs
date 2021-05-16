8bdd820ee39cc9aca189729cce017b9078082d18
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-15 20:31:31 -0400

    Added PauseMenu script
    Added PauseMenu script

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button btnQuit;
+
+    private Menu menu;
+
+    static private bool isPaused = false;
+
+    static public bool IsPaused { get { return isPaused; } }
+
+    void Awake()
+    {
+        isPaused = false;
+
+        menu = GetComponent<Menu>();
+
+        btnQuit.onClick.AddListener(onQuit);
+    }
+
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.P)) TogglePause();
+    }
+
+    public void Pause()
+    {
+        GameManager.singleton.SetCursorMode(false);
+
+        menu.Open();
+
+        Time.timeScale = 0f;
+
+        isPaused = true;
+    }
+
+    public void Unpause()
+    {
+        GameManager.singleton.SetCursorMode(true);
+
+        menu.Close();
+
+        Time.timeScale = 1f;
+
+        isPaused = false;
+    }
+
+    public void TogglePause()
+    {
+        if (isPaused) Unpause();
+        else Pause();
+    }
+
+    void onQuit()
+    {
+        Unpause();
+
+        GameManager.singleton.ChangeLevel("Main");
+    }
+}
