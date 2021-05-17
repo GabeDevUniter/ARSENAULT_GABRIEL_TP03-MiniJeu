b8da6f3fa55383e48a36b7f4e57e6f6b2977fbba
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-17 19:05:14 -0400

    Update SirenController.cs
    Added summary

--- Assets/Scripts/SirenController.cs
+++ Assets/Scripts/SirenController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controlling the police sirens
+/// </summary>
 public class SirenController : MonoBehaviour
 {
     [Header("Lights")]
