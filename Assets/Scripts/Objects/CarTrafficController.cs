b31e9f1b8d859c655c6ee9311d2797c235a22314
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-17 19:04:39 -0400

    Added CarTrafficController script
    Added CarTrafficController script, which will play an animation of a random car passing by on a single lane of the road

---
+++ Assets/Scripts/Objects/CarTrafficController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTrafficController : MonoBehaviour
+{
+    [SerializeField]
+    private float minInterval = 4f;
+
+    [SerializeField]
+    private float maxInterval = 7f;
+
+    [SerializeField]
+    private bool isOn = true;
+
+    private Animator[] animators;
+
+    private bool inProgress = false;
+
+    void Awake()
+    {
+        animators = GetComponentsInChildren<Animator>();
+    }
+
+    private void LateUpdate()
+    {
+        if (!inProgress && isOn) StartCoroutine(StartTraffic());
+    }
+
+    IEnumerator StartTraffic()
+    {
+        inProgress = true;
+
+        while(isOn)
+        {
+            pickCar().SetTrigger("Pass");
+
+            yield return new WaitForSeconds(Random.Range(minInterval, maxInterval));
+        }
+
+        inProgress = false;
+    }
+
+    Animator pickCar()
+    {
+        Animator pickedCar = animators[Random.Range(0, animators.Length)];
+
+        foreach(Animator car in animators)
+        {
+            car.gameObject.SetActive(car == pickedCar);
+        }
+
+        return pickedCar;
+    }
+}
