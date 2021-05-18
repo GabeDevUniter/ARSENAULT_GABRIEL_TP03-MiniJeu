71070b25fb7a769effe55db22dd2986e3ba27ad4
GabeDevUniter <79542610+GabeDevUniter@users.noreply.github.com>
2021-05-17 20:54:50 -0400

    Update CarTrafficController.cs
    Implemented a fully functional car traffic

--- Assets/Scripts/Objects/CarTrafficController.cs
+++ Assets/Scripts/Objects/CarTrafficController.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class CarTrafficController : MonoBehaviour
 {
     [SerializeField]
+    private bool isOn = true;
+
+    [SerializeField]
     private float minInterval = 4f;
 
     [SerializeField]
     private float maxInterval = 7f;
 
     [SerializeField]
-    private bool isOn = true;
+    private float speed = 0.6f;
 
     private Animator[] animators;
 
@@ -20,6 +23,10 @@ public class CarTrafficController : MonoBehaviour
     void Awake()
     {
         animators = GetComponentsInChildren<Animator>();
+
+        foreach (Animator car in animators) car.speed = speed;
+
+        SetVisibleAll(false);
     }
 
     private void LateUpdate()
@@ -33,23 +40,39 @@ public class CarTrafficController : MonoBehaviour
 
         while(isOn)
         {
-            pickCar().SetTrigger("Pass");
+            Animator pickedCar = pickCar();
+
+            yield return new WaitForSeconds(1f / speed);
+
+            pickedCar.gameObject.SetActive(false);
 
             yield return new WaitForSeconds(Random.Range(minInterval, maxInterval));
         }
 
+        foreach (Animator car in animators) car.gameObject.SetActive(false);
+
         inProgress = false;
     }
 
+    private List<Animator> carPicker = new List<Animator>();
     Animator pickCar()
     {
-        Animator pickedCar = animators[Random.Range(0, animators.Length)];
+        carPicker.Clear();
 
-        foreach(Animator car in animators)
-        {
-            car.gameObject.SetActive(car == pickedCar);
-        }
+        foreach (Animator car in animators)
+            if (!car.gameObject.activeInHierarchy) carPicker.Add(car);
+
+        if (carPicker.Count == 0) return default;
+
+        Animator pickedCar = carPicker[Random.Range(0, animators.Length)];
+
+        pickedCar.gameObject.SetActive(true);
 
         return pickedCar;
+    }
+
+    void SetVisibleAll(bool isVisible)
+    {
+        foreach (Animator car in animators) car.gameObject.SetActive(isVisible);
     }
 }
